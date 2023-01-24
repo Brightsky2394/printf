20c696e19d7a3d010c8c45eb55d14ddbf1279ae6
Dalex702 <102964343+Dalex702@users.noreply.github.com>
2023-01-24 02:55:01 +0100

    Update main.h

--- main.h
+++ main.h
@@ -18,6 +18,7 @@ typedef struct print
 	int (*f)(va_list, char *, unsigned int);
 } print_t;
 int _printf(const char *format, ...);
+int print_string(va_list l, flags_t *f)
 int print_prg(va_list __attribute__((unused)), char *, unsigned int);
 int print_chr(va_list arguments, char *buf, unsigned int ibuf);
 int print_str(va_list arguments, char *buf, unsigned int ibuf);
